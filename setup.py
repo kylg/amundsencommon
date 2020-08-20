79957f21d2cd7c851a7fcc86e7f1ffc4c626d20e
Jerry Zhu <jerry.zhu@electrolux.com>
2020-08-20 21:20:40 +0200

    update version to 0.3.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='amundsencommon-azure',
-    version='0.3.6',
+    version='0.3.8',
     description='Common code library for Amundsen',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
